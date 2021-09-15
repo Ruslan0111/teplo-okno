9ea219991f31adc264568aa6694e421ef32a8ed3
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-15 18:30:41 +0500

    header provided

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  document.querySelector('.header__burger').addEventListener('click', function() {
+    document.querySelector('.header__menu').classList.add('active');
+  })
+
+   document.querySelector('.header__menu-close').addEventListener('click', function() {
+    document.querySelector('.header__menu').classList.remove('active');
+  })
+};
