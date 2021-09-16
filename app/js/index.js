5c748ea7910e97e7416c36312032d9c6c81fd903
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-17 00:12:59 +0500

    main slider provided

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,25 @@
 window.onload = function() {
   document.querySelector('.header__burger').addEventListener('click', function() {
     document.querySelector('.header__menu').classList.add('active');
-  })
+  });
 
    document.querySelector('.header__menu-close').addEventListener('click', function() {
     document.querySelector('.header__menu').classList.remove('active');
-  })
+  });
+
+  let menu = ['-10%', '-15%', '-20%', '-35%']
+
+  let swiper = new Swiper(".section-slider__inner", {
+    navigation: {
+      nextEl: ".section-slider__button-next",
+      prevEl: ".section-slider__button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="rt-bold ' + className + '"><span>' + (menu[index]) + '</span></span>';
+      },
+    },
+  });
 };
