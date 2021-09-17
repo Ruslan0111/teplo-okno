5f053d3a86ab614c86b4505eac0268d843e3be5e
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-17 12:24:10 +0500

    complex-preparation section provided

--- app/js/index.js
+++ app/js/index.js
@@ -9,6 +9,7 @@ window.onload = function() {
 
   let slider1Menu = ['-10%', '-15%', '-20%', '-35%']
   let slider2Menu = ['-10%', '-10%', '-10%', '-30%', '-30%', '-50%']
+  let slider3Menu = ['ПОДАРОК', '-10%', '-20%', '-30%', '-30%', '-50%']
 
   let slider1 = new Swiper(".winter-mode__slider", {
     navigation: {
@@ -34,6 +35,20 @@ window.onload = function() {
       clickable: true,
       renderBullet: function (index, className) {
         return '<span class="rt-bold ' + className + '"><span>' + (slider2Menu[index]) + '</span></span>';
+      },
+    },
+  });
+
+  let slider3 = new Swiper(".complex-preparation__slider", {
+    navigation: {
+      nextEl: ".section-slider__button-next",
+      prevEl: ".section-slider__button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="rt-bold ' + className + '"><span>' + (slider3Menu[index]) + '</span></span>';
       },
     },
   });
