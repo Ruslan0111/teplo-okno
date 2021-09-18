1145db2074d043a385a797d2eed7b180e9191f9c
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-18 13:54:12 +0500

    special-orders section provided

--- app/js/index.js
+++ app/js/index.js
@@ -7,11 +7,11 @@ window.onload = function() {
     document.querySelector('.header__menu').classList.remove('active');
   });
 
-  let slider1Menu = ['-10%', '-15%', '-20%', '-35%']
-  let slider2Menu = ['-10%', '-10%', '-10%', '-30%', '-30%', '-50%']
-  let slider3Menu = ['ПОДАРОК', '-10%', '-20%', '-30%', '-30%', '-50%']
+  let mainSlider1Menu = ['-10%', '-15%', '-20%', '-35%']
+  let mainSlider2Menu = ['-10%', '-10%', '-10%', '-30%', '-30%', '-50%']
+  let mainSlider3Menu = ['ПОДАРОК', '-10%', '-20%', '-30%', '-30%', '-50%']
 
-  let slider1 = new Swiper(".winter-mode__slider", {
+  let mainSlider1 = new Swiper(".winter-mode__slider", {
     navigation: {
       nextEl: ".section-slider__button-next",
       prevEl: ".section-slider__button-prev",
@@ -20,12 +20,12 @@ window.onload = function() {
       el: ".swiper-pagination",
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="rt-bold ' + className + '"><span>' + (slider1Menu[index]) + '</span></span>';
+        return '<span class="rt-bold ' + className + '"><span>' + (mainSlider1Menu[index]) + '</span></span>';
       },
     },
   });
 
-  let slider2 = new Swiper(".big-control__slider", {
+  let mainSlider2 = new Swiper(".big-control__slider", {
     navigation: {
       nextEl: ".section-slider__button-next",
       prevEl: ".section-slider__button-prev",
@@ -34,12 +34,12 @@ window.onload = function() {
       el: ".swiper-pagination",
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="rt-bold ' + className + '"><span>' + (slider2Menu[index]) + '</span></span>';
+        return '<span class="rt-bold ' + className + '"><span>' + (mainSlider2Menu[index]) + '</span></span>';
       },
     },
   });
 
-  let slider3 = new Swiper(".complex-preparation__slider", {
+  let mainSlider3 = new Swiper(".complex-preparation__slider", {
     navigation: {
       nextEl: ".section-slider__button-next",
       prevEl: ".section-slider__button-prev",
@@ -48,8 +48,21 @@ window.onload = function() {
       el: ".swiper-pagination",
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="rt-bold ' + className + '"><span>' + (slider3Menu[index]) + '</span></span>';
+        return '<span class="rt-bold ' + className + '"><span>' + (mainSlider3Menu[index]) + '</span></span>';
       },
+    },
+  });
+
+  let ordersSlider = new Swiper(".special-orders__slider", {
+    navigation: {
+      nextEl: ".special-orders__slider-button-next",
+      prevEl: ".special-orders__slider-button-prev",
+    },
+    pagination: {
+      el: ".special-orders__slider-pagination",
+      clickable: true,
+      bulletClass: "special-orders__slider-pagination-bullet",
+      bulletActiveClass: "special-orders__slider-pagination-bullet-active",
     },
   });
 };
